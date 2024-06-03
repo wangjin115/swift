3652e74e0f505f81e992c791ad76e9cea407f327
wangjin115 <1150553544@qq.com>
2024-06-03 13:51:44 +0900

    1

--- hello/hello/ViewController.swift
+++ hello/hello/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(titleView.text)
+        titleView.text="代码设置"
         // Do any additional setup after loading the view.
     }
 
