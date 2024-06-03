73a73d76ef6d3a235ff8e18808ad23312269537d
wangjin115 <1150553544@qq.com>
2024-06-03 17:09:26 +0900

    1

--- hello/hello/ViewController.swift
+++ hello/hello/ViewController.swift
@@ -12,18 +12,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleView: UILabel!
     
+    @IBOutlet weak var buttonView: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(titleView.text)
         titleView.text="代码设置"
+        
+        buttonView.addTarget(self, action: #selector(buttonClick2(_:)), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
-
-     
+    
+    
     @IBAction func buttonClick(_ sender: UIButton) {
         
         print("点击一下")
+        
+        sender.setTitle("点过了", for: .normal)
     }
+    
+    @objc func buttonClick2(_ sender: UIButton) {
+        
+        print("点击一下22")
+    }
+    
 }
-
